2b6f722d83f824d72c78c8c3d8fa19f1343df2a1
Ryohei Seto <setoryohei@me.com>
2012-03-21 10:00:44 +0100

    first commit

---
+++ demsd/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  demsd
+//
+//  Created by Ryohei SETO on 12/03/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
