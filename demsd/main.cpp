102bbcdaddb3235295ac943a534475670abc801a
Ryohei Seto <setoryohei@me.com>
2012-03-21 10:03:30 +0100

    test commit for git and dropbox

--- demsd/main.cpp
+++ demsd/main.cpp
@@ -13,6 +13,8 @@ int main(int argc, const char * argv[])
 
     // insert code here...
     std::cout << "Hello, World!\n";
+    std::cout << "We'll create the codes to simulate restructuring \
+    of colloidal aggregate\n";
     return 0;
 }
 
